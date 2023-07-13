d3ec1951a1c96a52ab0d2a74b5feb57be2deabba
Mxoleli <20531662+Mxoleli@users.noreply.github.com>
2023-07-13 19:36:07 +0200

    First commit

---
+++ hangman.js
@@ -0,0 +1,19 @@
+/*
+Project: Hangman
+Author: Mxoleli Cwati
+*/
+
+//*************Project starts************//
+
+import inquirer from "inquirer";
+import data from './data';
+
+const [words, state] = data;
+//starting variables
+word = randomWord
+guessesRemaining =10
+
+//Verify the input, eg don't accept numbers
+askForLetter:
+    if inputNotLetter
+        dont
